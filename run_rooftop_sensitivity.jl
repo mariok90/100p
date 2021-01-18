4f6b042b1e5842573fc6c51806fc0e5a393eb9b6
mak <mak@wip.tu-berlin.de>
2021-01-18 15:10:37 +0100

    adjusted job files for rooftop sensitivity

--- run_rooftop_sensitivity.jl
+++ run_rooftop_sensitivity.jl
@@ -18,16 +18,49 @@ function main(limit)
         @aside _[1,:value] = 180 * limit
         CSV.write(scen_file, _)
     end
+
+    path_pot_base = joinpath("conditionalData", "potentialBase", "par_potential_DE.csv")
+    if limit > 1
+        rooftop_potential = @chain path_pot_base begin
+            CSV.read(_)
+            DataFrame
+            filter(x-> x.parameter == "capaConvUp" && x.technology_1 == "rooftop", _)
+            groupby(:region_2)
+            combine("value" => sum => "value")
+            zip(_.region_2, _.value)
+            Dict
+        end
+
+        df_new_pot = @chain path_pot_base begin
+            CSV.read(_)
+            DataFrame
+            filter(x-> x.parameter == "capaConvUp" && x.technology_2 == "rooftop_c", _)
+            transform(["region_2","value"] => ByRow((r,v)-> (limit - 1) * rooftop_potential[r] + v) => "value")
+        end
+
+        df_orig = CSV.read(path_pot_base) |> DataFrame
+        a = df_orig.parameter .== "capaConvUp"
+        b = df_orig.technology_2 .== "rooftop_c"
+        df_orig[a .& b, :] .= df_new_pot
+
+        pot_path = joinpath(path, "par_potential_DE.csv")
+        CSV.write(pot_path, df_orig)
+    else
+        pot_path = path_pot_base
+    end
         
     inDir = [
         "baseData",
         "scenarios/decentral",
         "conditionalData/lowerEE_DE",
         "timeSeries",
-        "conditionalData/potentialBase",
         "conditionalData/fixEU_potentialBase_grid",
         path
     ]
+
+    if pot_path == path_pot_base
+        push!(inDir, pot_path)
+    end
 
     out_dir = joinpath("_results", "rooftop")
     isdir(out_dir) || mkpath(out_dir)
@@ -58,7 +91,7 @@ end
 
 println("Starting Job")
 scen = ARGS[1]
-limit = 1 - ((parse(Int, scen) - 1) * 0.05)
+limit = parse(Int, scen) * 0.05
 
 println("Limit is $(100 * limit) %")
 println("Real limit is $(limit * 180)")
