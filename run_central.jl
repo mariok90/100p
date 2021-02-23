d4821a1ecdeab33512586c137d33bee1aa15aa6b
mak <mak@wip.tu-berlin.de>
2021-02-23 17:35:02 +0100

    desintegriertes szenario

--- run_central.jl
+++ run_central.jl
@@ -1,18 +1,23 @@
-using Gurobi, AnyMOD, CSV
+using Gurobi, AnyMOD
+using DataFrames, CSV
+include("anymod_postprocessing.jl")
 
-include("functions.jl")
-
-eePot = ARGS[1]
-gridExp = ARGS[2]
-engTech = ARGS[3]
-
-engTech_arr = engTech == "both" ? ["ocgtHydrogen","electrolysis","grid"] :  (engTech == "ocgt" ? ["ocgtHydrogen","electrolysis"] : (engTech == "bothNoE" ? ["ocgtHydrogen","grid","electrolysis"] : (engTech == "battery" ? ["grid","electrolysis"] : String[])))
-
+indir = [
+    "baseData",
+    "scenarios/copper",
+    "conditionalData/lowerEE_DE",
+    "timeSeries/demand",
+    "timeSeries/avail",
+    "conditionalData/potentialBase",
+    "conditionalData/fixEU_potentialBase_grid",
+]
 
 # * solve as copperplate
-model_object = anyModel(["baseData","scenarios/copper","conditionalData/lowerEE_DE","timeSeries","conditionalData/" * eePot, "conditionalData/fixEU_" * eePot * "_" * gridExp],"_results", objName = "central1_" * eePot * "_" * gridExp * "_" * engTech, bound = (capa = NaN, disp = NaN, obj = 2e7), decommExc  = :decomm);
-
-deRegions_arr = vcat([6],model_object.sets[:R].nodes[6].down);
+model_object = anyModel(
+    indir,
+    "_results/intermediate",
+    objName = "desintegriert_intermediate"
+);
 
 createOptModel!(model_object);
 setObjective!(:costs, model_object);
@@ -25,62 +30,59 @@ optimize!(model_object.optModel);
 reportResults(:summary,model_object);
 reportResults(:exchange,model_object);
 reportResults(:costs,model_object);
-plotSankey(model_object, "DE");
-plotSankey(model_object, "ENG");
 
-# * obtain capacities for technologies and write to parameter file
-eeSym_arr = filter(x -> model_object.parts.tech[x].type == :mature &&  keys(model_object.parts.tech[x].carrier)  |> (y -> :gen in y && !(:use in y)), collect(keys(model_object.parts.tech)));
-eeId_arr = map(x -> filter(y -> y.val == string(x),collect(values(model_object.sets[:Te].nodes)))[1].idx , eeSym_arr);
-noEeSym_arr = filter(x -> !(x in eeSym_arr) && model_object.parts.tech[x].type == :mature, collect(keys(model_object.parts.tech)));
-nonEeId_arr = map(x -> filter(y -> y.val == string(x),collect(values(model_object.sets[:Te].nodes)))[1].idx , noEeSym_arr);
+summary_df = reportResults(:summary,model_object, rtnOpt=(:csvDf,))
+expand_col!(summary_df, "technology")
+expand_col!(summary_df, "region_dispatch")
 
-allNuts3_arr = getfield.(filter(x -> x.lvl == 2 && model_object.sets[:R].up[x.idx] == 6, collect(values(model_object.sets[:R].nodes))),:idx);
+filter!(x-> x.variable == :capaConv, summary_df)
+filter!(x-> occursin("DE", x.region_dispatch_1), summary_df)
+tech_filter = ["wind_offshore", "wind_onshore", "pv"]
+tech3 = ["facade","agrar_pv"]
+filter!(x-> x.technology_1 in tech_filter, summary_df)
+transform!(
+    summary_df,
+    "technology_3" => ByRow(x-> x in tech3 ? missing : x) => "technology_3"
+)
+select!(
+    summary_df,
+    :region_dispatch_2 => :region_2,
+    :technology_1,
+    :technology_2,
+    :technology_3,
+    :value
+)
 
-# get share of total ee production per region
-eeVlh_dic = Dict((filter(y -> y.val == string(x),collect(values(model_object.sets[:Te].nodes)))[1].idx,r) => sum(filter(x -> x.R_dis == r,model_object.parts.tech[x].par[:avaConv].data)[!,:val]) for x in eeSym_arr, r in allNuts3_arr);
+df_low = transform(summary_df, "value" => ByRow(x-> floor(x, digits=4)) => "value")
+df_low[!,"parameter"] .= "capaConvLow"
 
-eeGen_df = filter(x -> x.variable == :capaConv && x.Te in eeId_arr && x.R_dis in allNuts3_arr,reportResults(:summary,model_object, rtnOpt = (:rawDf,)));
-eeGen_df[!,:gen] = map(x -> eeVlh_dic[(x.Te,x.R_dis)]*x.value,eachrow(eeGen_df));
-eeReg_df = combine(groupby(eeGen_df,:R_dis),:gen => (x -> sum(x)) => :gen);
-eeShare_dic = Dict(x.R_dis => x.gen/sum(eeReg_df[!,:gen]) for x in eachrow(eeReg_df));
+df_up = transform(summary_df, "value" => ByRow(x-> ceil(x, digits=4)) => "value")
+df_up[!,"parameter"] .= "capaConvUp"
 
-# convert computed capacities into fixed limits in a parameter inputfile
-eeCapa_df = filter(x -> x.variable == :capaConv && x.Te in eeId_arr && x.R_dis in allNuts3_arr,reportResults(:summary,model_object, rtnOpt = (:rawDf,)));
-
-# for non-ee technologies, capacities are distributed among subregions proportional to ee generation
-nonEECapa_df = filter(x -> x.variable in (:capaConv,:capaStIn,:capaStOut,:capaStSize) && x.Te in nonEeId_arr && x.R_dis == 6,reportResults(:summary,model_object, rtnOpt = (:rawDf,)));
-nonEECapa_df[!,:R_dis] = map(x -> model_object.sets[:R].nodes[x].down, nonEECapa_df[!,:R_dis]);
-nonEECapa_df = flatten(nonEECapa_df,:R_dis)
-nonEECapa_df[!,:value] = map(x -> eeShare_dic[x.R_dis]*x.value, eachrow(nonEECapa_df));
+CSV.write("intermediate/par_capalow.csv", df_low)
+CSV.write("intermediate/par_capaup.csv", df_up)
 
 
-# write final csv file that is used as an input for the next computation
-fixCapa_df = vcat(eeCapa_df,nonEECapa_df);
-fixCapa_df[!,:Te] = map(x -> model_object.sets[:Te].nodes[x].val,fixCapa_df[!,:Te]);
-if engTech != "all"
-    fixCapa_df[!,:parameter] .= map(x -> x.Te in engTech_arr ? Symbol(x.variable,:Low) : Symbol(x.variable,:Fix), eachrow(fixCapa_df));
-else
-    fixCapa_df[!,:parameter] .= map(x -> Symbol(x.variable,:Low), eachrow(fixCapa_df));
-end
-fixCapa_df[!,:region_2] = map(x -> model_object.sets[:R].nodes[x].val,fixCapa_df[!,:R_dis]);
-fixCapa_df[!,:technology_1] = map(x -> any(occursin.(["openspace_","rooftop_","onshore_","offshore_","home","grid"],x)) ? "" : x, fixCapa_df[!,:Te]);
-fixCapa_df[!,:technology_2] = map(x -> !any(occursin.(["openspace_","rooftop_","onshore_","offshore_","home","grid"],x)) ? "" : x, fixCapa_df[!,:Te]);
-fixCapa_df[!,:value] = map(x -> x > 1.00E-04 ? x : 0.0, fixCapa_df[!,:value])
+indir = [
+    "baseData",
+    "scenarios/decentral",
+    "intermediate",
+    "timeSeries/demand",
+    "timeSeries/avail",
+    "conditionalData/fixEU_potentialBase_grid",
+]
 
-fixCapa_df = leftjoin(fixCapa_df,model_object.parts.lim.par[:capaConvUp].data,on = [:Te => :Te, :R_dis => :R_exp])
-fixCapa_df[!,:value] = map(x -> ismissing(x.val) ? x.value : min(x.val,x.value), eachrow(fixCapa_df))
+model_object = anyModel(
+    indir,
+    "_results/desintegriert",
+    objName = "desintegriert"
+);
 
-select!(fixCapa_df, Not([:Ts_disSup,:R_dis,:C,:Te,:variable,:val]));
-
-CSV.write("conditionalData/intermediate_" * eePot * "_" * gridExp * "_" * engTech * "/par_fixCapa.csv", fixCapa_df);
-
-# * solve again with regions and exchange expansion, but with fixed capacities
-model_object = anyModel(["baseData","scenarios/decentral","timeSeries","conditionalData/" * eePot, "conditionalData/fixEU_" * eePot * "_" * gridExp,"conditionalData/intermediate_" * eePot * "_" * gridExp * "_" * engTech],"_results", objName = "central2_" * eePot * "_" * gridExp * "_" * engTech, bound = (capa = NaN, disp = NaN, obj = 2e7), decommExc  = :decomm);
 
 createOptModel!(model_object);
 setObjective!(:costs, model_object);
 
-set_optimizer(model_object.optModel, Gurobi.Optimizer);
+set_optimizer(model_object.optModel,Gurobi.Optimizer);
 set_optimizer_attribute(model_object.optModel, "Method", 2);
 set_optimizer_attribute(model_object.optModel, "Crossover", 0);
 optimize!(model_object.optModel);
@@ -88,17 +90,3 @@ optimize!(model_object.optModel);
 reportResults(:summary,model_object);
 reportResults(:exchange,model_object);
 reportResults(:costs,model_object);
-reportTimeSeries(:electricity, model_object);
-plotSankey(model_object, "DE");
-plotSankey(model_object, "ENG");
-
-# * changes to objective to maximize decentral generation and fixed all cost-relevant variables and variables outside of Germany
-changeObj!(model_object,deRegions_arr)
-
-optimize!(model_object.optModel);
-reportResults(:summary,model_object);
-reportResults(:exchange,model_object);
-reportResults(:costs,model_object);
-reportTimeSeries(:electricity, model_object)
-plotSankey(model_object, "DE");
-plotSankey(model_object, "ENG");
